5be6d885a1fa0cc03bdd70e3721b1887d6cb1455
maiws <maiara.sd@live.com>
2020-06-04 23:50:27 -0300

    Frontend do Ecoleta

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+//JSX - Syntax de xml dentro do Javascript
+import Routes from './routes';
+
+function App() {
+  //const [counter, setCounter] = useState(0); //Retorna Array / Primeira posição valor do estado, Segunda Posição atualizar o valor do estado
+
+  return (
+    <Routes />
+    );
+}
+
+export default App;
