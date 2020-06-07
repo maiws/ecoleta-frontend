29db268a238bd06f9a706a5148d3730407484d0b
maiws <maiara.sd@live.com>
2020-06-07 02:15:28 -0300

    ultimo dia

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -6,6 +6,8 @@ import api from '../../services/api';
 import {LeafletMouseEvent} from 'leaflet';
 import axios from 'axios';
 
+import  Dropzone from '../../components/Dropzone';
+
 // import { Container } from './styles';
 import './styles.css'
 import logo from '../../assets/logo.svg'
@@ -32,6 +34,8 @@ const CreatePoint = () => {
     const [cities, setCities] = useState<string[]>([]);
 
     const [initialPosition, setInitialPosition] = useState<[number, number]>([0,0]);
+
+    const [selectedFile, setSelectedFile] = useState<File>();
 
     const [formData, setFormData] = useState({
         name: '',
@@ -124,22 +128,28 @@ const CreatePoint = () => {
   async function handleSubmit(event: FormEvent){
     event.preventDefault();
 
+
     const { name, email, whatsapp } = formData;
     const uf = selectedUf;
     const city = selectedCity;
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-        name,
-        email,
-        whatsapp,
-        uf, 
-        city,
-        latitude,
-        longitude,
-        items
-    };
+    const data = new FormData();
+
+
+        data.append('name',name);
+        data.append('email',email);
+        data.append('whatsapp',whatsapp);
+        data.append('uf',uf);
+        data.append('city',city);
+        data.append('latitude',String(latitude));
+        data.append('longitude',String(longitude));
+        data.append('items',items.join(','));
+        
+        if(selectedFile){
+            data.append('image', selectedFile);
+        }
 
    await api.post('points', data);
 
@@ -160,6 +170,8 @@ const CreatePoint = () => {
         </header>
         <form action="" onSubmit={handleSubmit}>
             <h1>Cadastro do <br/> ponto de coleta</h1>
+
+            <Dropzone onFileUploaded={setSelectedFile}/>
 
             <fieldset>
                 <legend>
